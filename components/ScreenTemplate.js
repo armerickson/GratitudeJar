5f50689c71736895c526246dff6e02c62f9f7a75
armerickson <104520822+armerickson@users.noreply.github.com>
2022-09-26 13:36:03 -0500

    Changed apk name and made status bar transparent

--- components/ScreenTemplate.js
+++ components/ScreenTemplate.js
@@ -1,19 +1,25 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
 import colors from '../misc/colors';
 
 //Gradient background for all screens
 function ScreenTemplate ({ children }) {
  
   return (
-    <LinearGradient 
-    colors={[colors.ORANGE, colors.YELLOW]}
-    style={styles.container}
-    start={{ x: 0, y: 0 }}
-    end={{ x: 1, y: 1 }}
-    >
-      {children}
-    </LinearGradient>
+    <>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <LinearGradient 
+      colors={[colors.ORANGE, colors.YELLOW]}
+      style={styles.container}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      >
+        {children}
+      </LinearGradient>
+    </>
   )
 }
 export default ScreenTemplate;
